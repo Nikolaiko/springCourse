2c0d87592799f7ac069cd8f5a4f8ee12e75b1bbc
Nikolai <nbaklanov@issart.com>
2020-05-22 21:43:11 +0600

    Implemented POJO

---
+++ PetClinic/src/main/java/com/nikolai/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.nikolai.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ PetClinic/src/main/java/com/nikolai/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.nikolai.sfgpetclinic.model;
+
+public class Vet extends Person {
+    private PetType petType;
+}

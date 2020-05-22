d06ca76d565ba42fd018f27da658b95ce113ced9
Nikolai <nbaklanov@issart.com>
2020-05-22 23:16:02 +0600

    BaseEntity Interfaces for services

---
+++ PetClinic/pet-clinic-data/src/main/java/com/nikolai/sfgpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package com.nikolai.sfgpetclinic.services;
+
+import com.nikolai.sfgpetclinic.model.Pet;
+import com.nikolai.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
